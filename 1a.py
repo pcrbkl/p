48a855506fbdffce3ba9de70a14ef142593331da
RAHULGB <117293201+Rahul-Bhandage@users.noreply.github.com>
2024-03-21 13:18:33 +0530

    Update 1a.py

--- 1a.py
+++ 1a.py
@@ -1,7 +1,10 @@
 m1 = int(input("Enter marks for test1 : "))
 m2 = int(input("Enter marks for test2 : "))
 m3 = int(input("Enter marks for test3 : "))
-best_of_two = sorted([m1, m2, m3], reverse=True)[:2]
-average_best_of_two = sum(best_of_two)/2
-    
-print("Average of best two test marks out of three test’s marks is", average_best_of_two);
+if m1 <= m2 and m1 <= m3:
+    avgMarks = (m2 + m3) / 2
+elif m2 <= m1 and m2 <= m3:
+    avgMarks = (m1 + m3) / 2
+elif m3 <= m1 and m2 <= m2:
+    avgMarks = (m1 + m2) / 2
+print("Average of best two test marks out of three test’s marks is", avgMarks)
